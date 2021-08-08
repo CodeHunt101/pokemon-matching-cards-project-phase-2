60dc13031465521a40a984ddd35d3a42945349da
SET mentor <setmentor_parttime02@academyxi.com>
2021-08-08 14:18:24 +1000

    disable when open

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Card({pokemon, handleClick, index, isCardOpen}) {
 
   return (
-    <button onClick={(e)=>handleClick(e,index)} className={'card'}>
+    <button onClick={(e)=>handleClick(e,index)} className={'card'} disabled={isCardOpen} >
       <img className={isCardOpen ? 'open' : 'hidden'} src={pokemon} alt={pokemon}></img>
     </button>
   )
