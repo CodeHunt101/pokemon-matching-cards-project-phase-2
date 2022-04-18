cdc65ec499d64c23247b34aaa80d3d71b9145a62
Harold Torres Marino <haroldtm55@gmail.com>
2022-04-18 10:34:44 +1000

    Migrated <Card /> from js to ts

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,12 +1,20 @@
 import React from "react"
 
+type CardProps = {
+  pokemon: string
+  index: number
+  handleClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, index: number) => void
+  isCardOpen: boolean
+  disableCardIndicator: number
+}
+
 export default function Card({
   pokemon,
   handleClick,
   index,
   isCardOpen,
   disableCardIndicator,
-}) {
+}:CardProps) {
   return (
     <button
       onClick={(e) => handleClick(e, index)}
